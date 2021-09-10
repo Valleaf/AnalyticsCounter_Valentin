ebe5ee53920e61f4302467391a772424c2d4f096
Valentin <trouillet.valentin@gmail.com>
2021-09-10 14:30:17 +0200

    Reorganized

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,31 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    public static void main(String[] args) {
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking the user for the filename containing the symptoms
+        System.out.println("Please enter the filsymptoms.txte name");
+        String filename = scanner.nextLine();
+        scanner.close();
+        ReadSymptomDataFromFile rSymptomDataFromFile = new ReadSymptomDataFromFile(filename);
+
+        // Analyse the file and store its content it in a List of String
+        List<String> list = rSymptomDataFromFile.getSymptoms();
+
+        // Get the different symptoms by removing the duplicates
+        TreeMap<String, Integer> sortedSymptomsAndOccurences = rSymptomDataFromFile.getUniqueSymptomNames(list);
+
+        // Output the results in a file
+        ExtractDataFromText extractor = new ExtractDataFromText(sortedSymptomsAndOccurences);
+        extractor.extractData();
+
+    }
+
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 /**
  * Simple brute force implementation
@@ -13,35 +16,51 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
+	public List<String> getSymptoms() {
+		ArrayList<String> result = new ArrayList<>();
+
 		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
+			try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+
 				String line = reader.readLine();
-				
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
 				}
-				reader.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.err.println(
+						"File not found. Please check the filenames and try again. Filepath given : " + filepath);
 			}
 		}
-		
+
 		return result;
+	}
+
+	@Override
+	public TreeMap<String, Integer> getUniqueSymptomNames(List<String> symptoms) {
+		// Converting a List to a HashMap will remove all duplicates and allow us to
+		// count the number of elements.
+		HashMap<String, Integer> symptomsAndOccurences = new HashMap<>();
+		for (String symptom : symptoms) {
+			symptomsAndOccurences.put(symptom, Collections.frequency(symptoms, symptom));
+		}
+
+		// Converting the results to a TreeMap will automatically sort the elements
+		// alphabetically.
+		TreeMap<String, Integer> sortedSymptoms = new TreeMap<>();
+		sortedSymptoms.putAll(symptomsAndOccurences);
+		return sortedSymptoms;
 	}
 
 }
